0993688c120217f7777aca8ddb7b08a34174b9e8
Igor Gilyazov <gilyazovigorperm@gmail.com>
2019-03-05 22:39:37 +0500

    add year of building by address betta

--- longitude_and_latitude/Form1.Designer.cs
+++ longitude_and_latitude/Form1.Designer.cs
@@ -33,6 +33,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button_open_xlsx = new System.Windows.Forms.Button();
             this.button_save_xlsx = new System.Windows.Forms.Button();
+            this.button_year_by_address = new System.Windows.Forms.Button();
+            this.textBox_year = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -48,7 +50,7 @@
             this.button_start.Name = "button_start";
             this.button_start.Size = new System.Drawing.Size(146, 35);
             this.button_start.TabIndex = 1;
-            this.button_start.Text = "Пуск";
+            this.button_start.Text = "Пуск широта и долгота";
             this.button_start.UseVisualStyleBackColor = true;
             this.button_start.Click += new System.EventHandler(this.button_start_Click);
             // 
@@ -81,18 +83,37 @@
             this.button_save_xlsx.UseVisualStyleBackColor = true;
             this.button_save_xlsx.Click += new System.EventHandler(this.button_save_xlsx_Click);
             // 
+            // button_year_by_address
+            // 
+            this.button_year_by_address.Location = new System.Drawing.Point(3, 103);
+            this.button_year_by_address.Name = "button_year_by_address";
+            this.button_year_by_address.Size = new System.Drawing.Size(147, 37);
+            this.button_year_by_address.TabIndex = 5;
+            this.button_year_by_address.Text = "Год по адресу";
+            this.button_year_by_address.UseVisualStyleBackColor = true;
+            this.button_year_by_address.Click += new System.EventHandler(this.button_year_by_address_Click);
+            // 
+            // textBox_year
+            // 
+            this.textBox_year.Location = new System.Drawing.Point(155, 115);
+            this.textBox_year.Name = "textBox_year";
+            this.textBox_year.Size = new System.Drawing.Size(100, 20);
+            this.textBox_year.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(261, 104);
+            this.ClientSize = new System.Drawing.Size(261, 147);
+            this.Controls.Add(this.textBox_year);
+            this.Controls.Add(this.button_year_by_address);
             this.Controls.Add(this.button_save_xlsx);
             this.Controls.Add(this.button_open_xlsx);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button_start);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Долгота и широта по адресу";
+            this.Text = "Шир и долг по адресу";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,6 +126,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button_open_xlsx;
         private System.Windows.Forms.Button button_save_xlsx;
+        private System.Windows.Forms.Button button_year_by_address;
+        private System.Windows.Forms.TextBox textBox_year;
     }
 }
 
--- longitude_and_latitude/Form1.cs
+++ longitude_and_latitude/Form1.cs
@@ -50,6 +50,72 @@ namespace longitude_and_latitude
             else return;
         }
 
+        private void button_year_by_address_Click(object sender, EventArgs e)
+        {
+            if (path == "")
+            {
+                MessageBox.Show("Выберите excel-файл");
+                return;
+            }
+
+            if (new_path == "")
+            {
+                MessageBox.Show("Выберите директорию для сохранения");
+                return;
+            }
+
+            wb = excel.Workbooks.Open(path);
+            ws = wb.Worksheets[1];
+
+            //сколько строк дано
+            int data_i = 2;
+            while (ws.Cells[data_i, 19].Value2 != null)
+            { data_i++; }
+            data_i -= 2;
+
+            //открытие браузера
+            string url_site_map = "http://" + "dom.mingkh.ru/search?address=%D0%95%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3%2C+" + "" +
+                "%D0%91%D0%B5%D0%B1%D0%B5%D0%BB%D1%8F%2C+117&searchtype=house";
+            Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            //Browser.Manage().Window.Maximize();
+            Browser.Navigate().GoToUrl(url_site_map);
+            Thread.Sleep(3000);
+
+            IWebElement adress_element, data_element;
+            adress_element = Browser.FindElement(By.Id("address"));
+
+
+            //сбор данных
+            for (int i = 2; i <= data_i + 1; i++)
+            {
+                string adress_i = ws.Cells[i, 19].Value2;
+                adress_element.SendKeys(OpenQA.Selenium.Keys.Control + "a" + OpenQA.Selenium.Keys.Delete);
+                adress_element.SendKeys("Екатеринбург, " + adress_i + OpenQA.Selenium.Keys.Enter);
+                Thread.Sleep(3000);
+
+                //координат может быть не дано
+                try
+                {
+                    //List<IWebElement> addressList;
+                    //addressList = Browser.FindElements(By.CssSelector(".c6e8ba5398-header--1_m0_")).ToList();
+
+                    //data_element = Browser.FindElement(By.ClassName("clipboard__content"));
+                    //string data_str = data_element.Text;
+                    //string shirota = data_str.Substring(0, data_str.Length - 11);
+                    //string dolgota = data_str.Substring(data_str.Length - 9, 9);
+
+                    //ws.Cells[i, 5].Value2 = shirota;
+                    //ws.Cells[i, 6].Value2 = dolgota;
+                }
+                catch { }
+
+
+                textBox_year.Text = (i - 1).ToString() + "/" + data_i;
+            }
+
+
+        }
+
         private void button_start_Click(object sender, EventArgs e)
         {
 
